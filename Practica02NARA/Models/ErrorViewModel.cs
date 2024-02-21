6730dd283aa15ee8d806ecfd9e1a4f3e6009ae3f
nestor.adonay.rodriguez.alberto <nestor.adonay.rodriguez.alberto@gmail.com>
2024-02-21 10:14:05 -0600

    Agregar archivos de proyecto.

---
+++ Practica02NARA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica02NARA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
